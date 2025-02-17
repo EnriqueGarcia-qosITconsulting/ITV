c9aa32fdb1dbbcaca0f57b3421aad1d21c66e1e8
Egararj <egararj@gmail.com>
2025-02-17 10:25:44 +0100

    corrección

--- src/controlador/VehiculoController.java
+++ src/controlador/VehiculoController.java
@@ -2,9 +2,7 @@ package controlador;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.LinkedList;
 import java.util.Queue;
 
